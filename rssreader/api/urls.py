73746937eb0ebe8e2351651ef9323be989b4df7d
Robert Groves <rwgdev@gmail.com>
2020-10-05 05:22:01 -0500

    Implement user login.

--- rssreader/api/urls.py
+++ rssreader/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from api import views
 
 urlpatterns = [
-    path('users/register', views.UserCreate.as_view())
+    path('users/register', views.UserCreate.as_view(), name='user_register'),
+    path('users/login', obtain_auth_token, name='user_login'),
+    path('test', views.TestView.as_view(), name='test'),
 ]
--- rssreader/api/views.py
+++ rssreader/api/views.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from api.serializers import UserSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Define the view to be used with  the user creation (registration) endpoint.
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny)
+    permission_classes = (AllowAny,)
+
+class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Just a test.'}
+        return Response(content)
