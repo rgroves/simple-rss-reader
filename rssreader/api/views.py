6e78637999db70ebbcaee1c1627dc786dde57351
Robert Groves <rwgdev@gmail.com>
2020-10-05 16:41:55 -0500

    Add code to pass test_user_register.

--- rssreader/api/views.py
+++ rssreader/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from api.serializers import UserSerializer
 
@@ -12,6 +13,18 @@ class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Override create to be able to return access_token registration call.
+        """
+        # Serialize incoming data
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # Create user, get user'stoken and send response.
+        self.perform_create(serializer)
+        token = Token.objects.create(user=serializer.instance)
+        return Response({'access_token': token.key}, status=status.HTTP_201_CREATED)
 
 class TestView(APIView):
     permission_classes = (IsAuthenticated,)
