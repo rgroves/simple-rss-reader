c6587960ed0a7ffd1ee5dd622346ee35d211c458
Robert Groves <rwgdev@gmail.com>
2020-10-05 18:57:50 -0500

    Add more tests for login endpoint.

--- rssreader/api/tests.py
+++ rssreader/api/tests.py
@@ -145,3 +145,95 @@ class UserLoginTest(TestCase):
         str_content = str(resp.content, encoding='utf8')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertRegex(str_content, expected_content_regex)
+
+    def test_user_login_must_provide_valid_credentials(self):
+        """
+        Test to ensure bad request if invalid credentials are provided in the post data.
+        """
+        bad_post_data = self.post_data.copy()
+        bad_post_data['password'] = 'invalid'
+        resp = self.client.post(self.endpoint, bad_post_data)
+        expected_content = {"non_field_errors":["Unable to log in with provided credentials."]}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_must_provide_user(self):
+        """
+        Test to ensure bad request if no user name value is provided in the post data.
+        """
+        resp = self.client.post(self.endpoint, {'username': '', 'password': 'te$ter#'})
+        expected_content = {"username":["This field may not be blank."]}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_must_provide_password(self):
+        """
+        Test to ensure bad request if no password value is provided in the post data.
+        """
+        resp = self.client.post(self.endpoint, {'username': 'tester', 'password': ''})
+        expected_content = {"password":["This field may not be blank."]}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_get_not_allowed(self):
+        """
+        Test to ensure that GET method is not allowed.
+        """
+        resp = self.client.get(self.endpoint)
+        expected_content = {"detail": "Method \"GET\" not allowed."}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_head_not_allowed(self):
+        """
+        Test to ensure that HEAD method is not allowed.
+        """
+        resp = self.client.head(self.endpoint)
+        expected_content = ''
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(str_content, expected_content)
+
+    def test_user_login_put_not_allowed(self):
+        """
+        Test to ensure that PUT method is not allowed.
+        """
+        resp = self.client.put(self.endpoint)
+        expected_content = {"detail": "Method \"PUT\" not allowed."}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_delete_not_allowed(self):
+        """
+        Test to ensure that DELETE method is not allowed.
+        """
+        resp = self.client.delete(self.endpoint)
+        expected_content = {"detail": "Method \"DELETE\" not allowed."}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_trace_not_allowed(self):
+        """
+        Test to ensure that TRACE method is not allowed.
+        """
+        resp = self.client.trace(self.endpoint)
+        expected_content = {"detail": "Method \"TRACE\" not allowed."}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
+
+    def test_user_login_patch_not_allowed(self):
+        """
+        Test to ensure that PATCH method is not allowed.
+        """
+        resp = self.client.patch(self.endpoint)
+        expected_content = {"detail": "Method \"PATCH\" not allowed."}
+        str_content = str(resp.content, encoding='utf8')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertJSONEqual(str_content, json.dumps(expected_content))
