56a612e6d94a73a274122c95760fdffea598f498
Robert Groves <rwgdev@gmail.com>
2020-10-05 18:30:44 -0500

    Add code to pass test_user_login.

--- rssreader/api/urls.py
+++ rssreader/api/urls.py
@@ -4,6 +4,6 @@ from api import views
 
 urlpatterns = [
     path('users/register', views.UserCreate.as_view(), name='user_register'),
-    path('users/login', obtain_auth_token, name='user_login'),
+    path('users/login', views.UserLogin.as_view(), name='user_login'),
     path('test', views.TestView.as_view(), name='test'),
 ]
--- rssreader/api/views.py
+++ rssreader/api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from api.serializers import UserSerializer
 
@@ -25,6 +26,19 @@ class UserCreate(generics.CreateAPIView):
         self.perform_create(serializer)
         token = Token.objects.create(user=serializer.instance)
         return Response({'access_token': token.key}, status=status.HTTP_201_CREATED)
+
+class UserLogin(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        """
+        Override create to be able to return user id during login call.
+        """
+        # Serialize incoming data
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        # Get user's token and send response.
+        token = Token.objects.get(user=user)
+        return Response({'access_token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
 
 class TestView(APIView):
     permission_classes = (IsAuthenticated,)
