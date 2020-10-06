a1a584c21042c3cb1f4dc366da9ef0236db30216
Robert Groves <rwgdev@gmail.com>
2020-10-06 01:21:11 -0500

    Wire up feeds/add to URLconf.

--- rssreader/api/urls.py
+++ rssreader/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/register', views.UserCreate.as_view(), name='user_register'),
     path('users/login', views.UserLogin.as_view(), name='user_login'),
     path('test', views.TestView.as_view(), name='test'),
+    path('feeds/add', views.FeedCreate.as_view(), name='feeds_add'),
 ]
