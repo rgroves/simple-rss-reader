817273e8bf352bed11323fe1d9861804e407f747
Robert Groves <rwgdev@gmail.com>
2020-10-05 21:25:30 -0500

    Create Feed model.

--- rssreader/api/models.py
+++ rssreader/api/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Feed(models.Model):
+    url = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
