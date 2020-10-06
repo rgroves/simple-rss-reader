096d51347d08ebd9905247a8e2bd21ec86cd82ba
Robert Groves <rwgdev@gmail.com>
2020-10-05 21:36:53 -0500

    Made feed url unique.

--- rssreader/api/models.py
+++ rssreader/api/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Feed(models.Model):
-    url = models.CharField(max_length=1000)
+    url = models.CharField(max_length=1000, unique=True)
     title = models.CharField(max_length=100)
     users = models.ManyToManyField(User)
 
